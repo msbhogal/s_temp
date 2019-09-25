87f5f73ca79294467774d4093911c18f87f150bb
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-09-25 11:33:34 +0530

    Finished implementation of User

--- bookclub.py
+++ bookclub.py
@@ -8,15 +8,37 @@ class Book:
         self.name = name
         self.author = author
         self.genres = genres
-        self.user = user
-        
+        self.added_by = user
 
 class User:
     def __init__(self, name):
         self.name = name
+        self.__books = []
+    
+    def add_book(self, name, author, genre):
+        b = Book(self, name, author, genre)
+        state['books'].append(b)
+        return b
 
+    def delete_book(self, book_name):
+        found = None
+        for idx, i in enumerate(state['books']):
+            if i.added_by == self and i.name == book_name:
+                found = idx
+        if found != None:
+            state['books'].pop(found)
+        else:
+            raise KeyError("Book not found")
+        
+    @property
+    def books(self):
+        for i in state['books']:
+            if i.added_by == self:
+                yield i
         
         
+        
+        
+        
+    
 
-        
-        
--- test_bookclub.py
+++ test_bookclub.py
@@ -7,15 +7,35 @@ import bookclub
 def user(request):
     return bookclub.User("test")
 
+
+@pytest.fixture
+def user_with_book(request):
+    u = bookclub.User("test")
+    u.add_book("Testing title", "Testing author", ['test_genre1', 'test_genre2'])
+    return u
+
+
 def test_addbook(user):
     assert not bookclub.state['books']
     b = user.add_book("The adventures of Sherlock Holmes",
                       "Arthur Conan Doyle",
                       ['Victorian', 'Mystery'])
+    assert b.added_by == user
     assert b.name == "The adventures of Sherlock Holmes"
     assert b.author == "Arthur Conan Doyle"
     assert b.genres == ['Victorian', 'Mystery']
     assert bookclub.state['books'][0] is b
+    
+def test_listbooks(user_with_book):
+    assert len(list(user_with_book.books)) == 1
+    
+
+def test_deletebooks(user_with_book):
+    assert len(list(user_with_book.books)) == 1
+    user_with_book.delete_book("Testing title") #UNSURE
+    assert len(list(user_with_book.books)) == 0
+    
+    
     
     
     
